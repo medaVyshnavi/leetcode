74f49fdeab4602eaa95b06ca0ca9ca74cc06dd73
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2025-06-26 16:44:02 +0900

    Time: 0 ms (100.00%) | Memory: 57.9 MB (46.92%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -3,18 +3,17 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    let i= 1;
-    let j =1;
+    let i= 0;
+    let j=0;
 
     while(i<=j && j<nums.length){
-        if(nums[j] !== nums[j-1]){
-            nums[i] = nums[j]
-            i=i+1;
-            j=j+1
-        }
-        else{
-            j=j+1
+        if(nums[j] === nums[j-1]){
+            j++
+        }else{
+            nums[i] = nums[j];
+            i++;
+            j++
         }
     }
-    return i
+    return i;
 };
