f10739fd4e03adf4a6a5d079acb0e4c4cb8855c7
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2025-06-30 07:41:29 +0900

    Time: 0 ms (100.00%) | Memory: 53.2 MB (79.83%) - LeetSync

--- 27-remove-element/remove-element.js
+++ 27-remove-element/remove-element.js
@@ -4,18 +4,17 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
-    let i=0 ;
-    let j= 0;
+    let i=0;
+    let j=0;
 
     while(j<nums.length){
-        if(nums[j] !== val){
-            nums[i] = nums[j]
-            i++;
+        if(nums[j] === val){
             j++
         }else{
+            nums[i] = nums[j]
             j++
+            i++
         }
     }
-    console.log(i)
     return i
 };
