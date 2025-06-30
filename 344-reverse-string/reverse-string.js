adbab52590a521d23f625ce9373bdfc7250e337f
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2025-07-01 07:09:34 +0900

    Time: 4 ms (10.50%) | Memory: 61.4 MB (39.86%) - LeetSync

--- 344-reverse-string/reverse-string.js
+++ 344-reverse-string/reverse-string.js
@@ -3,14 +3,13 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-    let i=0;
-    let j=s.length-1
+    let i =0 ;
+    let j =s.length-1 ;
 
     while(i < j){
         [s[i],s[j]] = [s[j],s[i]]
-        i++
+        i++;
         j--
     }
-
     return s
 };
