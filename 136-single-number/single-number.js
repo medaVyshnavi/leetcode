017ab6f0ff32922e871d8801f4f72a22bd38d6a5
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2025-07-10 07:01:02 +0900

    Time: 0 ms (100.00%) | Memory: 55 MB (76.58%) - LeetSync

--- 136-single-number/single-number.js
+++ 136-single-number/single-number.js
@@ -3,19 +3,26 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    let hashMap = {};
+    let result = 0;
 
-    for(let i =0;i<nums.length;i++){
-        if(!hashMap[nums[i]]){
-            hashMap[nums[i]] = 1;
-        }else{
-            hashMap[nums[i]]++;
-        }
+    for(let i=0;i<nums.length;i++){
+        result ^= nums[i]
     }
+    return result;
 
-    for(let i =0;i<nums.length;i++){
-         if(hashMap[nums[i]] == 1){
-            return nums[i]
-        }
-    }
+
+    // let hashMap = {};
+    // for(let i =0;i<nums.length;i++){
+    //     if(!hashMap[nums[i]]){
+    //         hashMap[nums[i]] = 1;
+    //     }else{
+    //         hashMap[nums[i]]++;
+    //     }
+    // }
+
+    // for(let i =0;i<nums.length;i++){
+    //      if(hashMap[nums[i]] == 1){
+    //         return nums[i]
+    //     }
+    // }
 };
