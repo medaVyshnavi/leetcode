82e6e20b3df3e6101544de09aafd0703f7c28fb5
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2025-07-22 07:22:04 +0900

    Time: 68 ms (22.27%) | Memory: 53 MB (83.32%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.js
+++ 1013-fibonacci-number/fibonacci-number.js
@@ -3,9 +3,7 @@
  * @return {number}
  */
 var fib = function(n) {
-    let arr = [0,1];
-    for(let i=2;i<=n;i++){
-        arr[i] = arr[i-1] + arr[i-2]
-    }
-    return arr[n]
+    if(n===0) return 0;
+    if(n===1) return 1;
+    return fib(n-1) + fib(n-2)
 };
