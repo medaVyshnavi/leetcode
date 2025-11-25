b0c7890c7d558881eb1000f217109fb970ce6f64
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2025-11-25 20:49:57 +0900

    Time: 2 ms (71.74%) | Memory: 60 MB (53.91%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.js
+++ 485-max-consecutive-ones/max-consecutive-ones.js
@@ -3,16 +3,16 @@
  * @return {number}
  */
 var findMaxConsecutiveOnes = function(nums) {
-    let occurances = 0;
-    let maxOcc = 0;
+    let count = 0;
+    let maxCount = 0;
 
     for(let i=0;i<nums.length;i++){
         if(nums[i] === 1){
-            occurances++;
+            count++
         }else{
-            maxOcc = Math.max(maxOcc,occurances)
-            occurances = 0;
+            maxCount = Math.max(count,maxCount);
+            count = 0;
         }
     }
-    return Math.max(maxOcc,occurances)
+    return Math.max(count,maxCount)
 };
