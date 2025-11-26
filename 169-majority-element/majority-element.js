700e9aa4cb05521f4be353dfc76ef975b677f0fe
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2025-11-26 20:31:59 +0900

    Time: 3 ms (63.58%) | Memory: 58 MB (46.98%) - LeetSync

--- 169-majority-element/majority-element.js
+++ 169-majority-element/majority-element.js
@@ -3,21 +3,33 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    let limit = Math.floor(nums.length/2);
-    let hashMap = {};
+    let candidate = nums[0];
+    let count = 0;
 
-    for(let i=0; i<nums.length;i++){
-        if(!hashMap[nums[i]]){
-            hashMap[nums[i]] = 1;
-        }else{
-            hashMap[nums[i]]++;
+    for(const n of nums){
+        if(count === 0){
+            candidate = n
         }
+        count = candidate === n ? count+1 : count-1;
+        
     }
+    return candidate
 
-    for(let i=0;i<nums.length;i++){
-        if(hashMap[nums[i]] > limit){
-            return nums[i]
-        }
-    }
-    
+    // 0(n) time and space complexity
+    // let limit = Math.floor(nums.length/2);
+    // let hashMap = {};
+
+    // for(let i=0; i<nums.length;i++){
+    //     if(!hashMap[nums[i]]){
+    //         hashMap[nums[i]] = 1;
+    //     }else{
+    //         hashMap[nums[i]]++;
+    //     }
+    // }
+
+    // for(let i=0;i<nums.length;i++){
+    //     if(hashMap[nums[i]] > limit){
+    //         return nums[i]
+    //     }
+    // }
 };
