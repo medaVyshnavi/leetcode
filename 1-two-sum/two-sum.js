047dc153e59e16b4ff7ccb6330692f22256963f3
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2025-11-26 21:04:05 +0900

    Time: 0 ms (100.00%) | Memory: 57.6 MB (29.95%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,12 +4,23 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    for(let i=0;i<nums.length; i++){
-        for(let j=i+1; j<nums.length; j++){
-            let val = target - nums[j];
-            if(val === nums[i]){
-                return [i,j]
-            }
+    let map = new Map();
+    for(let i=0;i<nums.length;i++){
+        let val = target - nums[i];
+        if(map.has(val)){
+            return [map.get(val), i]
         }
+        map.set(nums[i],i)
     }
+
+
+
+    // for(let i=0;i<nums.length; i++){
+    //     for(let j=i+1; j<nums.length; j++){
+    //         let val = target - nums[j];
+    //         if(val === nums[i]){
+    //             return [i,j]
+    //         }
+    //     }
+    // }
 };
