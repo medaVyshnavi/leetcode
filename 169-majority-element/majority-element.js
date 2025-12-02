fe745eca098e16cef90b906d6454d2b7d459732c
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2025-12-02 21:05:51 +0900

    Time: 6 ms (35.40%) | Memory: 57.5 MB (61.19%) - LeetSync

--- 169-majority-element/majority-element.js
+++ 169-majority-element/majority-element.js
@@ -3,33 +3,21 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    let candidate = nums[0];
-    let count = 0;
-
-    for(const n of nums){
-        if(count === 0){
-            candidate = n
+    let map = new Map();
+    for(let i=0;i<nums.length;i++){
+        if(map.has(nums[i])){
+            map.set(nums[i],map.get(nums[i])+1);
+        } else{
+            map.set(nums[i],1)
         }
-        count = candidate === n ? count+1 : count-1;
-        
     }
-    return candidate
-
-    // 0(n) time and space complexity
-    // let limit = Math.floor(nums.length/2);
-    // let hashMap = {};
-
-    // for(let i=0; i<nums.length;i++){
-    //     if(!hashMap[nums[i]]){
-    //         hashMap[nums[i]] = 1;
-    //     }else{
-    //         hashMap[nums[i]]++;
-    //     }
-    // }
-
-    // for(let i=0;i<nums.length;i++){
-    //     if(hashMap[nums[i]] > limit){
-    //         return nums[i]
-    //     }
-    // }
+    let count = 0;
+    let maxElement = 0;
+    for(let i=0;i<nums.length;i++){
+       if(count < map.get(nums[i])){
+        count = map.get(nums[i]);
+        maxElement = nums[i]
+       }
+    }
+    return maxElement;
 };
