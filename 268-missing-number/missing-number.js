fc442aaad512aecaf123bfc15d439a53a6cc1aac
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2025-12-02 20:25:03 +0900

    Time: 0 ms (100.00%) | Memory: 57 MB (45.85%) - LeetSync

--- 268-missing-number/missing-number.js
+++ 268-missing-number/missing-number.js
@@ -3,15 +3,14 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
-    let sum = 0;
+    let xor = 0;
     for(let i=0;i< nums.length;i++){
-        sum+=nums[i]
+        xor^=nums[i]
     }
 
-    let totalSum = 0;
     for(let i=1; i<= nums.length;i++){
-        totalSum+= i;
+        xor^= i;
     }
 
-    return totalSum-sum;
+    return xor;
 };
