fac389ac39756cd031c7fb031c202049b1ef7964
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2025-12-02 22:13:10 +0900

    Time: 0 ms (100.00%) | Memory: 64.7 MB (38.81%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.js
+++ 53-maximum-subarray/maximum-subarray.js
@@ -6,13 +6,12 @@ var maxSubArray = function(nums) {
     let maxSum = -Infinity;
     let sum =0;
 
-    for(let i=0; i<nums.length;i++){
-        sum = sum + nums[i];
+    for(let i=0;i<nums.length;i++){
+        sum+=nums[i];
         maxSum = Math.max(maxSum,sum)
-
         if(sum < 0){
-            sum =0
+            sum =0;
         }
     }
-    return maxSum
+    return maxSum;
 };
