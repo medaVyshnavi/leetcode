2bbb115f67dd204951fa9ec9877cbda78f7267d0
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2025-12-08 22:22:09 +0900

    Time: 0 ms (100.00%) | Memory: 55 MB (58.76%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.js
+++ 782-jewels-and-stones/jewels-and-stones.js
@@ -4,21 +4,35 @@
  * @return {number}
  */
 var numJewelsInStones = function(jewels, stones) {
-    let count = 0;
-    let i=0;
-    let j=0;
+    let jSet = new Set();
+    for(let i=0;i<jewels.length;i++){
+        jSet.add(jewels[i]);
+    }
 
-    while(i<jewels.length){
-        if(jewels[i] === stones[j]){
-            count++;
-        }
-        j++;
-        if(j > stones.length-1){
-            i++;
-            j =0;
+    let count =0;
+    for(let i=0;i<stones.length;i++){
+        if(jSet.has(stones[i])){
+            count++
         }
     }
     return count;
+
+
+    // let count = 0;
+    // let i=0;
+    // let j=0;
+
+    // while(i<jewels.length){
+    //     if(jewels[i] === stones[j]){
+    //         count++;
+    //     }
+    //     j++;
+    //     if(j > stones.length-1){
+    //         i++;
+    //         j =0;
+    //     }
+    // }
+    // return count;
 
     // 1. nested for loops
     // let count = 0;
