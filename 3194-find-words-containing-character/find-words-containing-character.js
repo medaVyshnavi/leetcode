ad5c3f60398b89a35dae901c26d7ff92baa51532
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2025-12-08 21:18:22 +0900

    Time: 1 ms (83.58%) | Memory: 57.1 MB (87.36%) - LeetSync

--- 3194-find-words-containing-character/find-words-containing-character.js
+++ 3194-find-words-containing-character/find-words-containing-character.js
@@ -4,11 +4,27 @@
  * @return {number[]}
  */
 var findWordsContaining = function(s, x) {
-    let res= [];
-    for(let i=0;i<s.length;i++){
-        if(s[i].includes(x)){
-            res.push(i);
+    let res = [];
+    for(let i =0;i<s.length;i++){
+        let word = s[i];
+        let j =0;
+        while(j < word.length){
+            if(word[j] == x){
+                res.push(i);
+                break;
+            }
+            j++;
         }
     }
     return res;
+
+
+    // 1. builtin method
+    // let res= [];
+    // for(let i=0;i<s.length;i++){
+    //     if(s[i].includes(x)){
+    //         res.push(i);
+    //     }
+    // }
+    // return res;
 };
