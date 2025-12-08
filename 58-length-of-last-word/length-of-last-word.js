bc279aed902a7b2e2cc97ba867ff46fc17af67c0
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2025-12-08 20:15:56 +0900

    Time: 0 ms (100.00%) | Memory: 54.1 MB (57.71%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.js
+++ 58-length-of-last-word/length-of-last-word.js
@@ -3,7 +3,19 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-    s= s.trim();
-    s = s.split(" ");
-    return s[s.length-1].length;
+    let count = 0;
+    let i = s.length-1;
+    while(s[i] === " "){
+        i--;
+    }
+    while(s[i] !== " " && i>=0){
+        count++;
+        i--;
+    }
+    return count;
+
+
+    // s= s.trim();
+    // s = s.split(" ");
+    // return s[s.length-1].length;
 };
