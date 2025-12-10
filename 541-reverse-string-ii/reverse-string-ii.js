7f865d2f4c098b5edf76554eee772ce9f12ee3ce
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2025-12-11 06:21:44 +0900

    Time: 8 ms (16.11%) | Memory: 59.8 MB (8.16%) - LeetSync

--- 541-reverse-string-ii/reverse-string-ii.js
+++ 541-reverse-string-ii/reverse-string-ii.js
@@ -8,9 +8,8 @@
     let n = arr.length;
 
     for (let start = 0; start < n; start += 2 * k) {
-        // Reverse from start to start + k - 1 (or end of string)
-        let i = start;
-        let j = Math.min(start + k - 1, n - 1);
+        let i =start;
+        let j = Math.min(i + k - 1, n - 1);
 
         while (i < j) {
             [arr[i], arr[j]] = [arr[j], arr[i]];
