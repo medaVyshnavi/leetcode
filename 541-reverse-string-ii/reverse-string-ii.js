605bf946a56d89e04d933c3b919940fa02393025
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2025-12-11 05:58:29 +0900

    Time: 3 ms (74.82%) | Memory: 59.2 MB (13.70%) - LeetSync

--- 541-reverse-string-ii/reverse-string-ii.js
+++ 541-reverse-string-ii/reverse-string-ii.js
@@ -3,30 +3,22 @@
  * @param {number} k
  * @return {string}
  */
-var reverseStr = function(s, k) {
-    if(s.length < k) return s.split("").reverse().join("");
+    var reverseStr = function(s, k) {
+    let arr = s.split("");
+    let n = arr.length;
 
-    let m = 0;
-    let i=0;
-    let j = k-1;
-    s = s.split("")
-    while(j < s.length){
-        while(i < j){
-            [s[i],s[j]] = [s[j],s[i]];
-            i++;
-            j--;
-        }
-        m = m+( 2 *k);
-        i = m ;
-        j = m + k-1;
-    }
-    if(i<s.length && j>s.length){
-        j = s.length-1;
-        while(i < j){
-            [s[i],s[j]] = [s[j],s[i]];
+    for (let start = 0; start < n; start += 2 * k) {
+        // Reverse from start to start + k - 1 (or end of string)
+        let i = start;
+        let j = Math.min(start + k - 1, n - 1);
+
+        while (i < j) {
+            [arr[i], arr[j]] = [arr[j], arr[i]];
             i++;
             j--;
         }
     }
-    return s.join("");
+
+    return arr.join("");
+
 };
