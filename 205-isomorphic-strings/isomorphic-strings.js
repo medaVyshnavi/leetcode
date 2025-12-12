d241036a92c28202edc8d81f33907f8a79edefb4
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2025-12-12 21:04:26 +0900

    Time: 7 ms (49.43%) | Memory: 55.9 MB (52.99%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.js
+++ 205-isomorphic-strings/isomorphic-strings.js
@@ -11,11 +11,9 @@ var isIsomorphic = function(s, t) {
     for(let i=0;i<s.length;i++){
         if((sMap[s[i]] && sMap[s[i]]!== t[i]) || (tMap[t[i]] && tMap[t[i]]!== s[i])){
             return false;
-        } else{
-            sMap[s[i]] = t[i];
-            tMap[t[i]] = s[i]
-
-        }
+        } 
+        sMap[s[i]] = t[i];
+        tMap[t[i]] = s[i]
     }
     return true;
 };
