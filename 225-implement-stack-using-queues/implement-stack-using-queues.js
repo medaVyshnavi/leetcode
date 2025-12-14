808fa32b4c3acffea6e09fcab564dcf273426cca
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2025-12-15 07:28:36 +0900

    Time: 0 ms (100.00%) | Memory: 54.3 MB (31.97%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.js
+++ 225-implement-stack-using-queues/implement-stack-using-queues.js
@@ -1,7 +1,6 @@
 
 var MyStack = function() {
-    this.q1 = [];
-    this.q2 = [];
+    this.q = [];
 };
 
 /** 
@@ -9,44 +8,41 @@ var MyStack = function() {
  * @return {void}
  */
 MyStack.prototype.push = function(x) {
-    this.q1.push(x);
+    this.q.push(x)
 };
 
 /**
  * @return {number}
  */
 MyStack.prototype.pop = function() {
-    let n = this.q1.length
-    for(let i=0; i< n-1; i++){
-        this.q2.push(this.q1.shift());
+    let n = this.q.length;
+
+    for(let i=0; i<n-1;i++){
+        this.q.push(this.q.shift());
     }
-    let front = this.q1.shift();
-    [this.q1,this.q2] = [this.q2,this.q1];
-    
-    return front;
+
+    return this.q.shift();
 };
 
 /**
  * @return {number}
  */
 MyStack.prototype.top = function() {
-     let n = this.q1.length
+    let n = this.q.length;
     for(let i=0; i<n-1; i++){
-        this.q2.push(this.q1.shift());
+        this.q.push(this.q.shift());
     }
-
-    let front = this.q1[0];
-    this.q2.push(this.q1.shift());
-
-    [this.q1,this.q2] =[this.q2,this.q1];
-    return front
+    let front = this.q[0];
+    this.q.push(this.q.shift());
+    return front;
 };
 
 /**
  * @return {boolean}
  */
 MyStack.prototype.empty = function() {
-    return this.q1.length === 0
+    return this.q.length === 0
+    
 };
 
 /** 
