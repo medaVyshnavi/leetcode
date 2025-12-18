9f297f04df321b323031038945d3ffcedf1937f3
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2025-12-19 07:15:33 +0900

    Time: 1 ms (96.04%) | Memory: 58.3 MB (20.17%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.js
+++ 496-next-greater-element-i/next-greater-element-i.js
@@ -9,19 +9,11 @@ var nextGreaterElement = function(nums1, s) {
 
     let n = s.length;
     for(let i= n-1; i>=0;i--){
-        while(stack.length){
-            if(s[i] > stack[stack.length-1]){
-                stack.pop();
-            } else{
-                map.set(s[i], stack[stack.length-1]);
-                stack.push(s[i])
-                break;
-            }
+        while(stack.length && s[i] > stack[stack.length-1]){
+            stack.pop();
         }
-        if(!stack.length){
-            map.set(s[i], -1)
-            stack.push(s[i])
-        }
+        map.set(s[i], stack.length ? stack[stack.length-1] :-1)
+        stack.push(s[i])
     }
     return nums1.map(num => map.get(num))
 };
