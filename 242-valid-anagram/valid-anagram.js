0aee358dc959deb5c51d917ee4a1c426df1c4b3c
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2025-12-21 20:20:47 +0900

    Time: 23 ms (28.90%) | Memory: 57.7 MB (59.60%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -4,22 +4,39 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    if(s.length !== t.length) return false;
+    let map1 = new Map();
+    let map2 = new Map();
 
-    let map = {};
-    for(let i=0; i<s.length;i++){
-        if(map[s[i]]){
-            map[s[i]]++;
+    for(let i=0;i<s.length;i++){
+        if(map1.has(s[i])){
+            map1.set(s[i],map1.get(s[i])+1)
         }else{
-            map[s[i]] = 1
+            map1.set(s[i],1)
         }
     }
 
     for(let i=0;i<t.length;i++){
-        if(!map[t[i]] || map[t[i]] < 0){
-            return false
+        if(map2.has(t[i])){
+            map2.set(t[i],map2.get(t[i])+1)
+        }else{
+            map2.set(t[i],1)
         }
-        map[t[i]]--; 
     }
-    return true;
+
+    let res = Array(26).fill(0);
+    let res2 = Array(26).fill(0);
+    let val = [...map1.keys()];
+    let val2 = [...map2.keys()];
+
+    for(let i=0;i<val.length;i++){
+        let pos = val[i].charCodeAt(0) - 97;
+        res[pos] = map1.get(val[i])
+    }
+    
+    for(let i=0;i<val2.length;i++){
+        let pos = val2[i].charCodeAt(0) - 97;
+        res2[pos] = map2.get(val2[i])
+    }
+
+    return res.join("#") === res2.join("#")
 };
