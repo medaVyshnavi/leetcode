0410a96e9931b2787dd8468404810adf68a702de
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2025-12-21 20:52:24 +0900

    Time: 4 ms (23.76%) | Memory: 56.5 MB (19.45%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.js
+++ 14-longest-common-prefix/longest-common-prefix.js
@@ -3,17 +3,18 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-    let prefix = "";
-    let j=0;
+    let ref = strs[0];
+    let i=0;
+    let res= "";
 
-    while(j < strs[0].length){
-        for(let i =1; i<strs.length ; i++){
-            if(strs[i][j] !== strs[0][j] || j== strs[i].length){
-                return prefix;
+    while(i < ref.length){
+        for(let j=0;j<strs.length;j++){
+            if(strs[j][i] !== ref[i]){
+                return res;
             }
         }
-        prefix+= strs[0][j];
-        j++;
+        res+=ref[i];
+        i++
     }
-    return prefix;
+    return res;
 };
