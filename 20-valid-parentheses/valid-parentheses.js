5beaa91d527440b6a36de01752fe0b03202b1210
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2025-12-21 13:20:07 +0900

    Time: 2 ms (81.31%) | Memory: 54.6 MB (98.80%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.js
+++ 20-valid-parentheses/valid-parentheses.js
@@ -10,16 +10,15 @@ var isValid = function(s) {
         "[" : "]"
     }
 
-    for(let i=0;i<s.length;i++){
+    for(let i=0;i< s.length;i++){
         if(map[s[i]]){
             stack.push(s[i])
-        } else{
-            let front = stack.pop();
-            if(!front || map[front] !== s[i] ){
+        }else{
+            let top = stack.pop();
+            if(s[i] !== map[top]){
                 return false
             }
         }
     }
-    if(stack.length !== 0) return false;
-    return true;
+    return stack.length ===  0;
 };
