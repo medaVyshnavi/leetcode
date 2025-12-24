90dc9519f6adb7bedfde191e2b0b4808b6a85ba0
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2025-12-24 20:14:47 +0900

    Time: 43 ms (66.46%) | Memory: 53.7 MB (69.13%) - LeetSync

---
+++ 278-first-bad-version/first-bad-version.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        let left = 1;
+        let right = n;
+
+        while(left<=right){
+            let m = left + Math.floor((right -left)/2);
+            let res = isBadVersion(m);
+            if(!res){
+                left = m+1;
+            }else{
+                right= m-1;
+            }
+        }
+        return right+1;
+    };
+};
