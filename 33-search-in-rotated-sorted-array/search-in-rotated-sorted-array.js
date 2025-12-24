8e12ff655aa8beca8b267831d1899561cc991dc1
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2025-12-24 18:16:18 +0900

    Time: 0 ms (100.00%) | Memory: 54.3 MB (73.95%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
@@ -3,24 +3,24 @@
  * @param {number} target
  * @return {number}
  */
-var search = function(arr, target) {
+var search = function (arr, target) {
     let left = 0;
-    let right = arr.length-1;
+    let right = arr.length - 1;
 
-    while(left <= right){
-        let m = left + Math.floor((right-left)/2);
-        if(arr[m] === target) return m;
-        if(arr[m] >= arr[left]){
-            if((target < arr[m] && target < arr[left]) || (target > arr[m] && target > arr[left])){
-                left = m+1;
-            } else{
-                right = m-1;
+    while (left <= right) {
+        let m = left + Math.floor((right - left) / 2);
+        if (arr[m] === target) return m;
+        if (arr[m] >= arr[left]) {
+            if (target < arr[m] && target >= arr[left]) {
+                right = m - 1;
+            } else {
+                left = m + 1;
             }
-        } else{
-            if((target > arr[m] && target > arr[right]) || (target < arr[m] && target < arr[left])){
-                right = m-1;
-            } else{
-                left = m+1;
+        } else {
+            if (target > arr[m] && target <= arr[right]) {
+                left = m + 1;
+            } else {
+                right = m - 1;
             }
         }
     }
