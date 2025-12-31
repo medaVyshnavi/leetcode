cbde2587d30f839c9c8e8e7743332720d3029ed5
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2025-12-31 16:30:18 +0900

    Time: 5 ms (78.81%) | Memory: 55.9 MB (94.09%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let i = 0;
+    let j = 0;
+    let map = new Map();
+    let max = 0;
+
+    while(j < s.length){
+        if(!map.has(s[j])){
+            map.set(s[j], j)
+        } else{
+            let index = map.get(s[j]);
+            if( index >= i){
+                i = index+1;
+            }
+            map.set(s[j], j);
+        }
+        max = Math.max(max , j-i+1);
+        j++
+    }
+
+    return max
+
+};
