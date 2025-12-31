98ff773cd7ca1d4cd84861fcc60365078e805d68
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2025-12-31 16:30:59 +0900

    Time: 1 ms (99.55%) | Memory: 57.5 MB (62.96%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -2,26 +2,23 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function(s) {
+var lengthOfLongestSubstring = function (s) {
     let i = 0;
     let j = 0;
     let map = new Map();
     let max = 0;
 
-    while(j < s.length){
-        if(!map.has(s[j])){
-            map.set(s[j], j)
-        } else{
+    while (j < s.length) {
+        if (map.has(s[j])) {
             let index = map.get(s[j]);
-            if( index >= i){
-                i = index+1;
+            if (index >= i) {
+                i = index + 1;
             }
-            map.set(s[j], j);
         }
-        max = Math.max(max , j-i+1);
+        map.set(s[j], j);
+        max = Math.max(max, j - i + 1);
         j++
     }
-
     return max
 
 };
