2263c9cfbaddd9a10e491d5cf7af85f1b957beb1
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2026-01-05 20:30:08 +0900

    Time: 2 ms (49.14%) | Memory: 60.9 MB (22.45%) - LeetSync

--- 283-move-zeroes/move-zeroes.js
+++ 283-move-zeroes/move-zeroes.js
@@ -3,11 +3,15 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    let i =0;
-    for(let j=0; j< nums.length; j++){
+    let i=0;
+    for(let j=0;j<nums.length;j++){
         if(nums[j] !== 0){
-            [nums[i],nums[j]] = [nums[j],nums[i]];
-            i++;
+            nums[i] = nums[j];
+            i++
         }
+    }
+    while(i < nums.length){
+        nums[i] = 0;
+        i++
     }
 };
