de250f2dcbbab217f977a688ff1ba8a94f13a836
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2026-01-05 21:11:21 +0900

    Time: 2 ms (66.93%) | Memory: 58.5 MB (40.09%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.js
+++ 941-sort-array-by-parity/sort-array-by-parity.js
@@ -12,16 +12,3 @@ var sortArrayByParity = function(nums) {
     }
     return nums
 };
-
-var sortArrayByParity = function(nums) {
-    let i = 0;
-
-    for (let j = 0; j < nums.length; j++) {
-        if (nums[j] % 2 === 0) {
-            [nums[i], nums[j]] = [nums[j], nums[i]];
-            i++;
-        }
-    }
-
-    return nums;
-};
