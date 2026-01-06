0ce30973649bc926e52d38ab9aeede148937cd0b
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2026-01-06 22:09:18 +0900

    Time: 0 ms (100.00%) | Memory: 57.2 MB (6.61%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
@@ -3,18 +3,18 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(arr, target) {
-    let i = 0;
-    let j = arr.length-1;
+var twoSum = function(nums, target) {
+    let i =0;
+    let j= nums.length-1;
 
     while(i < j){
-        let sum = arr[i] + arr[j];
+        let sum = nums[i] + nums[j];
         if(sum == target){
             return [i+1,j+1];
         } else if(sum > target){
             j--;
-        } else{
+        }else{
             i++;
         }
-    }
+    } 
 };
