f04e6303a4a925374591bb53fbf443bf5e07c95c
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2026-01-11 12:07:19 +0900

    Time: 12 ms (58.53%) | Memory: 65 MB (67.05%) - LeetSync

---
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var numberOfSubarrays = function(nums, k) {
+    return helper(nums,k) - helper(nums,k-1)
+};
+
+function helper (nums,k){
+    let i = 0;
+    let j = 0;
+    let count = 0;
+    let odd = 0;
+    while(j < nums.length){
+        if(nums[j] % 2 === 1){
+            odd++
+        }
+        
+        while(odd > k){
+            if(nums[i] % 2 === 1){
+                odd--;
+            }
+            i++
+        }
+        count = count+(j-i+1);
+        j++
+    }
+    return count
+}
