fb7de325e483b4f756c531c572ef756ce2381d8a
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2026-01-12 11:36:45 +0900

    Time: 24 ms (64.88%) | Memory: 56.4 MB (87.46%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.js
+++ 76-minimum-window-substring/minimum-window-substring.js
@@ -4,42 +4,40 @@
  * @return {string}
  */
 var minWindow = function(s, t) {
-    if(t.length > s.length) return "";
-    let need = {};
-    let required = 0;
+    let need = {}
     for(let ch of t){
-        need[ch] = (need[ch] || 0)+1;
+        need[ch] = (need[ch] || 0) + 1;
     }
-    required = Object.keys(need).length;
-
+    let required = Object.keys(need).length;
+    let window = {}
     let i = 0;
-    let j= 0;
-    let satisfied = 0;
-    let window = {};
+    let j = 0;
     let start = 0;
-    let minLen = Infinity
+    let minLen = Infinity;
+    let matched = 0;
 
     while(j < s.length){
         let ch = s[j];
-        window[ch] = (window[ch] || 0)+1;
+        window[ch] = (window[ch] || 0 ) + 1;
         if(need[ch] && window[ch] === need[ch]){
-            satisfied++;
+            matched++
         }
-        
-        while(satisfied === required){
-            if(j - i +1 < minLen){
+
+        while(matched === required){
+            if(j - i + 1 < minLen){
                 minLen = j-i+1;
                 start = i
             }
             let c = s[i];
             if(need[c] && window[c] === need[c]){
-                satisfied--;
+                matched--;
             }
             window[c]--;
-            if(window[c] ===0) {delete window[c]}
-            i++
+            if(window[c] === 0) delete window[c]
+            i++;
         }
         j++;
     }
+
     return minLen === Infinity ? "" : s.substring(start,start+minLen)
 };
