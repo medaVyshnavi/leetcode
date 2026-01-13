82d3c88f671000b4dc907e0195d1b419f8e187f7
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2026-01-13 21:48:46 +0900

    Time: 2 ms (91.14%) | Memory: 55.8 MB (60.76%) - LeetSync

--- 389-find-the-difference/find-the-difference.js
+++ 389-find-the-difference/find-the-difference.js
@@ -4,19 +4,12 @@
  * @return {character}
  */
 var findTheDifference = function(s, t) {
-    let sArray = Array(26).fill(0);
+    let result = 0
     for(let ch of s){
-        sArray[ch.charCodeAt(0)-97]++;
+        result = result ^ ch.charCodeAt(0)
     }
-
-    let tArray = Array(26).fill(0);
     for(let ch of t){
-        tArray[ch.charCodeAt(0)-97]++;
+        result = result ^ ch.charCodeAt(0);
     }
-
-    for(let i =0; i < tArray.length; i++){
-        if(sArray[i] !== tArray[i]){
-            return String.fromCharCode(i+97)
-        }
-    }
+    return String.fromCharCode(result)
 };
