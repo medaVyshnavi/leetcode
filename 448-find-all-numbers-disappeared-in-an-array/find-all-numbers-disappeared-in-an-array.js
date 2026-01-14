cfcb948af8cbd01b3859a62939d6db9beb5e10bb
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2026-01-14 20:46:53 +0900

    Time: 8 ms (73.31%) | Memory: 71.8 MB (75.63%) - LeetSync

---
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDisappearedNumbers = function(nums) {
+    let res = Array(nums.length).fill(0);
+    for(let i =0; i<nums.length;i++){
+        res[nums[i]-1]++;
+    }
+    nums = [];
+    for(let i =0;i<res.length;i++){
+        if(res[i]== 0){
+            nums.push(i+1)
+        }
+    }
+    return nums
+};
