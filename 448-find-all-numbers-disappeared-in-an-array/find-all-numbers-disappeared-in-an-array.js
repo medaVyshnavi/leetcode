fafa277601ab1956f58b2a585e4d9763c2092637
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2026-01-14 21:18:55 +0900

    Time: 8 ms (73.31%) | Memory: 73.2 MB (56.28%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.js
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.js
@@ -3,17 +3,18 @@
  * @return {number[]}
  */
 var findDisappearedNumbers = function(nums) {
-    let res = Array(nums.length).fill(0);
-    for(let i =0; i<nums.length;i++){
-        res[nums[i]-1]++;
-    }
-    nums = [];
-    for(let i =0;i<res.length;i++){
-        if(res[i]== 0){
-            nums.push(i+1)
+    let res = [];
+    for(let i =0;i<nums.length;i++){
+        let index = Math.abs(nums[i])-1;
+        if(nums[index] > 0){
+            nums[index] = -nums[index]
         }
     }
-    return nums
+    
+    for(let i =0;i<nums.length;i++){
+        if(nums[i] > 0){
+            res.push(i+1);
+        }
+    }
+    return res
 };
-
-// o(n) -> TC and SC
