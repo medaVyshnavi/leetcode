a512b3e4dbedd059bd5aeaf961da57daf1059fad
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2026-01-17 18:12:11 +0900

    Time: 0 ms (100.00%) | Memory: 51.7 MB (99.70%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.js
+++ 58-length-of-last-word/length-of-last-word.js
@@ -6,31 +6,14 @@ var lengthOfLastWord = function(s) {
     let count = 0;
     let i = s.length-1;
 
-    while(i >=0){
-        if(s[i] !== " "){
-            count++
-        }
-        else if(count>0){
+    while(i >= 0){
+        if(count > 0 && s[i] == " "){
             break;
+        }
+        if(s[i] !== " "){
+            count++;
         }
         i--;
     }
-    return count;
-
-    // approach 2
-    // let count = 0;
-    // let i = s.length-1;
-    // while(s[i] === " "){
-    //     i--;
-    // }
-    // while(s[i] !== " " && i>=0){
-    //     count++;
-    //     i--;
-    // }
-    // return count;
-
-    // 1. using built in methods limited to javascript.
-    // s= s.trim();
-    // s = s.split(" ");
-    // return s[s.length-1].length;
+    return count
 };
