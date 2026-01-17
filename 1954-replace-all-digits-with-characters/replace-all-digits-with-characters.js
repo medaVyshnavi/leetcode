b4f35c20acf181f4d0352edcd01bdbf0f0ebad51
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2026-01-17 19:23:23 +0900

    Time: 3 ms (9.23%) | Memory: 53.8 MB (76.92%) - LeetSync

--- 1954-replace-all-digits-with-characters/replace-all-digits-with-characters.js
+++ 1954-replace-all-digits-with-characters/replace-all-digits-with-characters.js
@@ -4,18 +4,17 @@
  */
 var replaceDigits = function(s) {
     let res = [];
-
-    for (let i = 0; i < s.length; i++) {
-        if (i % 2 === 0) {
-            res.push(s[i]);
+    for(let i = 0;i<s.length;i++){
+        if(i % 2 === 0 ){
+            res.push(s[i])
         } else {
-            let shifted =
-                String.fromCharCode(
-                    s[i - 1].charCodeAt(0) + Number(s[i])
-                );
-            res.push(shifted);
-        }
+            res.push(shift(s[i-1],s[i]))
+        }   
     }
-
     return res.join("");
 };
+
+function shift(char,digit){
+    let val = char.charCodeAt(0) - 97 + Number(digit);
+    return (String.fromCharCode(val + 97));
+}
