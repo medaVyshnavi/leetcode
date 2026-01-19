7f9fd2e6b6e5c330b5699bfd83601cecad7c6d01
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2026-01-19 20:47:16 +0900

    Time: 2 ms (34.46%) | Memory: 55.9 MB (53.72%) - LeetSync

--- 69-sqrtx/sqrtx.js
+++ 69-sqrtx/sqrtx.js
@@ -4,18 +4,19 @@
  */
 var mySqrt = function(x) {
     if(x < 2) return x;
+
     let low = 2;
     let high = Math.floor(x/2);
 
-    while(low <= high){
+    while(low<=high){
         let mid = low + Math.floor((high-low)/2);
-        if(mid**2 == x) {
-            return mid;
-        } else if(mid**2 > x){
-            high= mid-1;
-        } else{
-            low = low+1;
+        if(mid**2 === x){
+            return mid
+        }else if(mid**2 > x){
+            high = mid-1;
+        }else{
+            low = mid+1;
         }
     }
-    return high;
+    return high
 };
