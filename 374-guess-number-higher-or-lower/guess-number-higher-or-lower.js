1b96de81ac79e72b07615d3726bbb353d43edec8
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2026-01-19 21:09:03 +0900

    Time: 43 ms (59.61%) | Memory: 53.7 MB (61.88%) - LeetSync

--- 374-guess-number-higher-or-lower/guess-number-higher-or-lower.js
+++ 374-guess-number-higher-or-lower/guess-number-higher-or-lower.js
@@ -12,18 +12,18 @@
  * @return {number}
  */
 var guessNumber = function(n) {
-    let l = 1;
-    let r = n;
+    let low = 1;
+    let high = n;
 
-    while(l <= r){
-        let m = l + Math.floor((r-l)/2);
-        let res = guess(m);
-        if(res ===0 ){
-            return m;
-        } else if(res === -1){
-            r = m-1;
+    while(low <= high){
+        let mid = low + Math.floor((high-low)/2);
+        let res = guess(mid)
+        if( res === 0){
+            return mid
+        }else if( res === -1 ){
+            high = mid -1;
         }else{
-            l = m+1
+            low = mid+1
         }
     }
 };
