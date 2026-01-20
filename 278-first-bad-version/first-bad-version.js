e658a4c4b48da2bc1e574e8ecf991ef033265656
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2026-01-20 20:54:59 +0900

    Time: 51 ms (15.28%) | Memory: 52.7 MB (94.17%) - LeetSync

--- 278-first-bad-version/first-bad-version.js
+++ 278-first-bad-version/first-bad-version.js
@@ -21,15 +21,15 @@ var solution = function(isBadVersion) {
         let left = 1;
         let right = n;
 
-        while(left<=right){
-            let m = left + Math.floor((right -left)/2);
-            let res = isBadVersion(m);
-            if(!res){
-                left = m+1;
+        while(left <= right){
+            let mid = left+Math.floor((right-left)/2)
+            let status = isBadVersion(mid);
+            if(status){
+                right = mid-1;
             }else{
-                right= m-1;
+                left = mid + 1;
             }
         }
-        return right+1;
+        return left
     };
 };
