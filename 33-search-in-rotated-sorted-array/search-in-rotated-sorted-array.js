4f544676cabf0f72e1c6d308e47222b7adee3d37
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2026-01-21 19:22:34 +0900

    Time: 0 ms (100.00%) | Memory: 53.9 MB (68.35%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
@@ -5,27 +5,23 @@
  */
 var search = function(nums, target) {
     let left = 0;
-    let right = nums.length -1;
-
+    let right = nums.length-1;
     while(left <= right){
-        let mid = left + Math.floor((right - left)/2);
+        let mid = left + Math.floor((right-left)/2);
         if(nums[mid] === target) return mid;
-
-        //left sorted
-        if(nums[mid] >= nums[left]){
+        if(nums[left] <= nums[mid]){
             if(target < nums[mid] && target >= nums[left]){
-                right = mid-1;
+                right = mid -1
             }else{
-                left = mid+1
-            }
-        } else{
-            if(target<= nums[right] && target > nums[mid]){
                 left = mid+1;
+            }
+        }else{
+            if(target > nums[mid] && target <= nums[right]){
+                left = mid+1
             }else{
-                right = mid-1
+                right = mid -1
             }
         }
-    
     }
-    return -1
+    return -1;
 };
