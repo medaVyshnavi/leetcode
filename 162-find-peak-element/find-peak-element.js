c67068879373563c7078a5c747625b766439b5c4
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2026-01-27 21:40:39 +0900

    Time: 0 ms (100.00%) | Memory: 53.6 MB (74.04%) - LeetSync

--- 162-find-peak-element/find-peak-element.js
+++ 162-find-peak-element/find-peak-element.js
@@ -4,14 +4,14 @@
  */
 var findPeakElement = function(nums) {
     let left = 0;
-    let right = nums.length -1;
+    let right = nums.length-1;
 
     while(left < right){
         let mid = left + Math.floor((right-left)/2);
-        if(nums[mid] > nums[mid+1]){
-            right = mid;
+        if(nums[mid] < nums[mid+1]){
+            left = mid +1;
         }else{
-            left = mid+1
+            right = mid
         }
     }
     return left
