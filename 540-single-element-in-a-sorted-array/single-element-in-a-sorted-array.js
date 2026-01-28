dd996d700f5e17e3fea95cd278e9184fe0adac31
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2026-01-28 20:01:10 +0900

    Time: 0 ms (100.00%) | Memory: 57.1 MB (76.04%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.js
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.js
@@ -2,26 +2,19 @@
  * @param {number[]} nums
  * @return {number}
  */
-var singleNonDuplicate = function(arr) {
-    let l = 0;
-    let r = arr.length-1;
-    while( l <= r){
-        let m = l + Math.floor((r-l)/2);
-        if(arr[m] !== arr[m-1] && arr[m] !== arr[m+1]){
-            return arr[m];
-        }
-        if(arr[m] === arr[m-1]){
-            if((m-1-l) % 2 !== 0){
-                r = m-2;
-            } else{
-                l = m+1;
-            }
-        } else{
-            if((r-m-1) %2 !==0){
-                l = m+2
-            }else{
-                r = m-1;
-            }
+var singleNonDuplicate = function(nums) {
+    let left = 0;
+    let right = nums.length-1;
+
+    while(left < right){
+        let mid = left + Math.floor((right-left)/2);
+        // force mid to be at the even index
+        if(mid % 2 === 1) mid--;
+        if(nums[mid] === nums[mid+1]){
+            left = mid+2
+        }else{
+            right = mid
         }
     }
+    return nums[left]
 };
