f4b02ab6074025c0b1087756bbc4e00bd427a582
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2026-02-05 21:02:34 +0900

    Time: 19 ms (49.38%) | Memory: 78.4 MB (66.08%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.js
+++ 739-daily-temperatures/daily-temperatures.js
@@ -3,17 +3,15 @@
  * @return {number[]}
  */
 var dailyTemperatures = function(s) {
-    let res = Array(s.length).fill(0);
+    let ans = Array(s.length).fill(0);
     let stack = [];
 
-    for(let i=s.length-1;i>=0;i--){
-        while(s.length && s[i] >= s[stack[stack.length-1]]){
-            stack.pop();
+    for(let i=0; i < s.length;i++){
+        while(stack.length > 0 && s[i] > s[stack[stack.length-1]]){
+            let element = stack.pop();
+            ans[element] = i - element;
         }
-        let top = stack[stack.length-1]
         stack.push(i)
-        res[i] = top ? top-i : 0;
     }
-    return res;
-
+    return ans
 };
