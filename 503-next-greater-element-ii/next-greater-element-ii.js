726d2aae4338c31d9b82e57075527a0a37a2915c
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2026-02-11 21:15:25 +0900

    Time: 8 ms (54.41%) | Memory: 61.1 MB (86.07%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.js
+++ 503-next-greater-element-ii/next-greater-element-ii.js
@@ -3,16 +3,16 @@
  * @return {number[]}
  */
 var nextGreaterElements = function(nums) {
-    let arr = Array(nums.length).fill(-1)
     let stack = [];
+    let res = Array(nums.length).fill(-1);
     let n = nums.length;
 
-    for(let i=(2*n)-1;i>=0;i--){
-        while(stack.length && nums[i%n]>= nums[stack[stack.length-1]]){
-            stack.pop()
+    for(let i = 0; i< (2*n)-1 ; i++){
+        while(stack.length && nums[i%n] > nums[stack[stack.length-1]]){
+            let idx = stack.pop();
+            res[idx] = nums[i%n];
         }
-        arr[i%n] = stack.length ? nums[stack[stack.length - 1]] :-1
         stack.push(i%n)
     }
-    return arr
+    return res;
 };
