2b93c6a0d67c19d5c749ab253b044286ff4fc01f
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2026-02-17 20:50:14 +0900

    Time: 4 ms (69.78%) | Memory: 55.9 MB (88.49%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.js
+++ 735-asteroid-collision/asteroid-collision.js
@@ -4,7 +4,8 @@
  */
 var asteroidCollision = function(nums) {
     let stack = [];
-    for(let i =0;i<nums.length;i++){
+    
+    for(let i =0; i<nums.length; i++){
         let alive = true;
         while(stack.length && nums[i] < 0 && stack[stack.length-1] > 0){
             if(Math.abs(nums[i]) > Math.abs(stack[stack.length-1])){
@@ -13,13 +14,14 @@ var asteroidCollision = function(nums) {
                 stack.pop();
                 alive = false;
                 break;
-            }
-            else{
-                alive = false
+            }else{
+                alive = false;
                 break;
             }
         }
-        if(alive) stack.push(nums[i])
+        if(alive){
+            stack.push(nums[i])
+        }
     }
     return stack
 };
