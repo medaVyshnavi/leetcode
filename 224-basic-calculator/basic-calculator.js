3fb6506db997f6638543c2d94b9e236033798474
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2026-02-18 21:03:14 +0900

    Time: 17 ms (70.82%) | Memory: 60 MB (67.83%) - LeetSync

---
+++ 224-basic-calculator/basic-calculator.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var calculate = function(s) {
+    let result = 0;
+    let number = 0;
+    let sign = 1;
+    let stack = [];
+
+    for(let i = 0;i< s.length; i++){
+        if(s[i] >= '0' && s[i] <= '9'){
+            number = number *10 + Number(s[i])
+        }else if(s[i] === "+"){
+            result = result + (sign * number)
+            number = 0;
+            sign = 1;
+        }else if(s[i] === "-"){
+            result = result + (sign * number)
+            number = 0;
+            sign= -1;
+        }else if(s[i] == "("){
+            stack.push(result);
+            stack.push(sign)
+            result = 0;
+            sign =1;
+        }else if(s[i] == ")"){
+            result = result + (sign*number);
+            number =0
+            let prevSign = stack.pop();
+            let prevResult = stack.pop();
+            result = prevResult + (prevSign * result)
+        }
+    }
+    result = result+ (sign * number)
+    return result
+};
