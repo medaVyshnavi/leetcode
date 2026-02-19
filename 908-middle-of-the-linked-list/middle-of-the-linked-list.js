683582b5e3299e425d1542d8ea4b064524740348
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2026-02-19 20:18:28 +0900

    Time: 0 ms (100.00%) | Memory: 53.2 MB (82.97%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.js
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.js
@@ -14,8 +14,8 @@ var middleNode = function(head) {
     let fast = head;
 
     while(fast && fast.next){
-        slow=slow.next;
         fast = fast.next.next;
+        slow = slow.next
     }
 
     return slow
