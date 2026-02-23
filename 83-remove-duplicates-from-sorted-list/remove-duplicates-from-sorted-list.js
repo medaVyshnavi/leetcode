119264ab2bb53dbbe89543111d5406de52907169
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2026-02-23 16:31:00 +0900

    Time: 0 ms (100.00%) | Memory: 58.1 MB (42.78%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.js
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.js
@@ -10,14 +10,14 @@
  * @return {ListNode}
  */
 var deleteDuplicates = function(head) {
+    if(!head) return head
     let current = head;
-
-    while(current && current.next){
+    while(current.next){
         if(current.val === current.next.val){
-            current.next = current.next.next
+            current.next = current.next.next;
         }else{
             current = current.next
         }
     }
-    return head
+    return head;
 };
