31d21d3201fb0cc57da7f2b5a38a5d07f57c14e4
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2026-02-24 21:21:09 +0900

    Time: 0 ms (100.00%) | Memory: 53.8 MB (66.27%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.js
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.js
@@ -9,24 +9,13 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var swapPairs = function(head) {
+var swapPairs = function(head) {           // recursive approach
     if(!head || !head.next) return head;
 
-    let start = new ListNode();
-    start.next = head;
-    let p = start;
-    let c = head;
-    let n = c.next;
+    let l = head;
+    let r = l.next;
 
-    while(c && n){
-        p.next = c.next;
-        c.next = n.next;
-        n.next = c;
-
-        p = c;
-        c = p.next;
-        n = c && c.next
-    }
-
-    return start.next
+    l.next = swapPairs(r.next);
+    r.next = l;
+    return r
 };
