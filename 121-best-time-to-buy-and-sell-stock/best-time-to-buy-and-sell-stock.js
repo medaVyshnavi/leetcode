7e2e14636c13a1d549e52bc3c21aa0f2eca0fbd5
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2026-08-07 03:51:17 +0530

    Time: 2 ms (80.75%) | Memory: 64.6 MB (88.53%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -3,13 +3,12 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let maxProfit = 0;
-    let minBuyPrice = Infinity;
-
-    for(let i =0; i<prices.length; i++){
-        minBuyPrice = Math.min(minBuyPrice,prices[i]);
-        maxProfit = Math.max(maxProfit,prices[i] - minBuyPrice)
-    };
-
-    return maxProfit;
+    let maxProfit = 0
+    let leastValue = prices[0]
+    for(let i= 0;i<prices.length;i++){
+        leastValue = leastValue > prices[i] ? prices[i] : leastValue;
+        let profit = prices[i] - leastValue;
+        maxProfit = maxProfit > profit ? maxProfit : profit
+    }
+    return maxProfit
 };
