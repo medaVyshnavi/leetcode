243f4de448fbdba8183c2f9c0fbc8eeae9b3af43
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2026-08-13 03:40:26 +0530

    Time: 1 ms (88.33%) | Memory: 56.6 MB (63.83%) - LeetSync

--- 169-majority-element/majority-element.js
+++ 169-majority-element/majority-element.js
@@ -3,18 +3,36 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    let map = new Map();
+    // O(1) time complexity and space complexity
+    let candidate = nums[0];
+    let votes = 0;
     for(let i=0;i<nums.length;i++){
-        if(!map.get(nums[i])){
-            map.set(nums[i],1)
-        }else{
-            map.set(nums[i],map.get(nums[i])+1)
+        if(candidate === nums[i]){
+            votes++
+        }else(
+            votes--
+        )
+        if(votes == 0){
+            votes = 0;
+            candidate = nums[i+1]
         }
     }
+    return candidate
 
-    for(let i=0;i<nums.length;i++){
-        if(map.get(nums[i]) > Math.floor(nums.length/2)){
-            return nums[i]
-        }
-    }
+
+    // o(1) Time complexity and o(n) space complexity because we are using Map
+    // let map = new Map();
+    // for(let i=0;i<nums.length;i++){
+    //     if(!map.get(nums[i])){
+    //         map.set(nums[i],1)
+    //     }else{
+    //         map.set(nums[i],map.get(nums[i])+1)
+    //     }
+    // }
+
+    // for(let i=0;i<nums.length;i++){
+    //     if(map.get(nums[i]) > Math.floor(nums.length/2)){
+    //         return nums[i]
+    //     }
+    // }
 };
