068d89a2fcd2118ec67af848e0930affbf535078
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2026-08-18 17:56:32 +0530

    Time: 2 ms (59.30%) | Memory: 56.7 MB (94.44%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.js
+++ 941-sort-array-by-parity/sort-array-by-parity.js
@@ -5,8 +5,8 @@
 var sortArrayByParity = function(nums) {
     let i = 0;
     for(let j=0;j<nums.length;j++){
-        if(nums[j] %2===0){
-            [nums[i],nums[j]] = [nums[j],nums[i]];
+        if(nums[j] % 2 == 0){
+            [nums[j],nums[i]] =  [nums[i],nums[j]];
             i++
         }
     }
