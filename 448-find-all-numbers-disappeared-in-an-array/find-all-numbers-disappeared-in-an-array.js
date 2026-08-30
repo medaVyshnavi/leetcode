536712ded99bd574e55001ac617cb301e94f289a
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2026-08-31 03:35:59 +0530

    Time: 13 ms (61.31%) | Memory: 70.9 MB (93.50%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.js
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.js
@@ -3,20 +3,18 @@
  * @return {number[]}
  */
 var findDisappearedNumbers = function(nums) {
-    let res= [];
-
-    for(let i=0;i<nums.length;i++){
-        let index = Math.abs(nums[i]) - 1
+    let res = [];
+    for(let i =0; i< nums.length;i++){
+        let index = Math.abs(nums[i]) - 1;
         if(nums[index] > 0){
             nums[index] = -nums[index]
-        }   
+        }
     }
-
-    for(let i=0; i< nums.length;i++){
+    console.log(nums)
+    for(let i=0;i<nums.length;i++){
         if(nums[i] > 0){
             res.push(i+1)
         }
     }
-
     return res
 };
