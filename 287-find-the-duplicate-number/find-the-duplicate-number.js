5cf8b422f6078e81d2b27c1f18c7970d4ba2b138
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2026-09-02 02:50:14 +0530

    Time: 5 ms (93.25%) | Memory: 62.6 MB (93.41%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.js
+++ 287-find-the-duplicate-number/find-the-duplicate-number.js
@@ -3,21 +3,19 @@
  * @return {number}
  */
 var findDuplicate = function(nums) {
-    let slow= nums[0];
+    let slow = nums[0];
     let fast = nums[0];
 
-    // cycle detection
     do{
         slow = nums[slow];
         fast = nums[nums[fast]]
-    } while(slow !== fast)
+    }while(slow != fast)
 
-    // finding duplicate. 
     slow = nums[0];
-    while(slow !== fast){
+    while(slow != fast){
         slow = nums[slow];
-        fast = nums[fast];
+        fast = nums[fast]
     }
 
-    return slow;
+    return slow
 };
