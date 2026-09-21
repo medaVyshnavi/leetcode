675da9c6a85ba1f8ed77e5c1f37cdacec6ba0bdc
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2026-09-22 04:50:25 +0900

    Time: 0 ms (100.00%) | Memory: 54.1 MB (18.24%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.js
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.js
@@ -11,15 +11,14 @@
  * @return {number[]}
  */
 var preorderTraversal = function(root) {
-    if(!root) return []
-    let res = [];
-    let stack = [root]
-    
+    if(!root) return [];
+    let ans = [];
+    let stack = [root];
     while(stack.length){
-        let curr = stack.pop()
-        res.push(curr.val);
+        let curr = stack.pop();
+        ans.push(curr.val);
         curr.right && stack.push(curr.right)
         curr.left && stack.push(curr.left)
     }
-    return res
+    return ans
 };
